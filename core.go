22ad3781163e72df559fa1454318711a7b967a72
Grzegorz Nosek <gnosek@git.igrid.tech>
2022-11-17 22:07:39 +0100

    init

---
+++ core.go
@@ -0,0 +1,23 @@
+package xmath
+
+import "fmt"
+
+func PrintHello() {
+	fmt.Println("Hello, Modules! This is xmath speaking!")
+}
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Div(a, b int) float64 {
+	return float64(a / b)
+}
+
+func Pow(a, b int) int {
+	base := a
+	for i := 1; i <= b; i++ {
+		a = a * base
+	}
+	return a
+}
